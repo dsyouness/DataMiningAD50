1d9e4dd0ffebecf176435ffb031f039882671ce8
iblladi <ismail.blladi@utbm.fr>
2019-01-08 01:31:25 +0100

    update code

--- AD50P.R
+++ AD50P.R
@@ -1,8 +1,12 @@
+#Librairie FactoMineR
 library(FactoMineR)
-library(gplots)
 library(factoextra)
+
+library(gplots)
 library(corrplot)
 
+#Librairie ADE4, Cluster
+library(cluster)
 library(ade4)
 
 bdtmp_multim = read.csv("~/AD50/AFC/budget_temps_multimedia.csv", header=TRUE, row.names=1, sep=";")
@@ -10,12 +14,12 @@ t_bdtmp_multim = as.table(as.matrix(bdtmp_multim))
 balloonplot(t(t_bdtmp_multim) ,main ="bdtmp_multim", xlab ="", ylab="",
             label = FALSE, show.margins = FALSE)
 
-df= bdtmp_multim[1:8,]
-
 ##############################   TEST KHI-DEUX  ########################################
 
-test <- chisq.test(bdtmp_multim)  
+test <- chisq.test(bdtmp_multim[1:8,])  
 test #p-value of 2.2*10^-16 <<< 0.01 donc il y a dépandance entre les variables lignes et colonnes
+
+
 test$statistic #: la statistique du Chi2.
 test$parameter #: le nombre de degrés de libertés.
 test$p.value #: la p-value.
@@ -24,25 +28,23 @@ test$expected #: la matrice attendue sous l'hypothèse nulle d'absence de biais.
 
 ##############################   AFC  ########################################
 
-res.ca <- CA(bdtmp_multim, row.sup = 9:nrow(bdtmp_multim))
+#Methode AFC FactoMineR
+res.ca <- CA(bdtmp_multim, row.sup = 9:nrow(bdtmp_multim),graph=FALSE)
 
-res.ca <- CA(bdtmp_multim[1:8,])
 
+#Methode AFC ADE4
 res.ca <- dudi.coa(bdtmp_multim[1:8,],scannf= FALSE, nf = 2) # hclust eclust
 
-row <- get_ca_row(res.ca)
-
-col <- get_ca_col(res.ca)
 
 
+#Plots
 fviz_screeplot(res.ca, addlabels = TRUE, ylim = c(0, 70)) # Inerties
 
-fviz_ca_biplot(res.ca, repel = TRUE) #Symetric
+fviz_ca_biplot(res.ca, repel = TRUE) #Sans fleches
 
-fviz_ca_biplot(res.ca, arrow = c(TRUE, TRUE),
-               repel = TRUE)
+fviz_ca_biplot(res.ca, arrow = c(TRUE, TRUE),repel = TRUE)#Avec fleches
 
-#Cos2 ROW
+#Cos² ROW
 
 fviz_ca_row(res.ca, col.row = "cos2",
             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), 
@@ -52,7 +54,7 @@ corrplot(row$cos2, is.corr=FALSE)
 
 fviz_cos2(res.ca, choice = "row", axes = 1:2)
 
-#Cos2 COL
+#Cos² COL
 
 fviz_ca_col(res.ca, col.col = "cos2", 
             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
@@ -63,7 +65,8 @@ fviz_cos2(res.ca, choice = "col", axes = 1:2)
 
 #Contribution
 
-corrplot(row$contrib, is.corr=FALSE) 
+corrplot(row$contrib, is.corr=FALSE)
+
 
 # Contributions of rows to dimension 1
 fviz_contrib(res.ca, choice = "row", axes = 1, top = 10)
@@ -81,7 +84,7 @@ fviz_ca_row(res.ca, col.row = "contrib",
 fviz_contrib(res.ca, choice = "col", axes = 1:2) #COL
 
 
-#Interpretation
+#Aides à l'interpretation
 
 print(res.ca)
 
@@ -91,6 +94,7 @@ summary(res.ca)
 
 ##############################   HCLUST / KMEANS / SILHOUETTE  ########################################
 
+#Methode CLUSTER FactoMiner
 clust0 <- HCPC(res.ca,consol = FALSE) #kmeans FALSE
 clust <- HCPC(res.ca,consol = TRUE) #kmeans TRUE
 
@@ -103,34 +107,48 @@ clust$desc.ind$para #Individus représentatifs de chaque groupe
 
 plot(clust, choice = "3D.map")
 
-fviz_cluster(clust,repel = TRUE,            # Evite le chevauchement des textes
+fviz_cluster(clust,repel = TRUE,  # Evite le chevauchement des textes
              show.clust.cent = TRUE, # Montre le centre des clusters
              palette = "jco",         # Palette de couleurs, voir ?ggpubr::ggpar
              ggtheme = theme_minimal(),
              main = "K-means")
 
 
+#Methode CLUSTER Factoextra 
+res.hc <- eclust(res.ca$li[,1:2], "hclust", k = 4,
+                 method = "ward.D2", graph = FALSE)
+fviz_dend(res.hc, rect = TRUE, show_labels = TRUE) 
 
 
+#Methode CLUSTER "Stats"
 dist <- dist(res.ca$li[,1:2],method="euclidian")
 dist <- dist^2
 classif <- hclust(d= dist, method="ward.D2")
 plot(classif, main="Dendrogramme \n methode de Ward", xlab = "Les professions",
      sub = "")
 
-kmeans <- kmeans(res.ca$li[,1:2], centers= 4 , nstart =2)
+#K-means marche avec AFC ADE4
+
+#K-means run = 1
+kmeans1 <- kmeans(res.ca$li[,1:2], centers= 4 , nstart =1)
 print(kmeans)
 
+#K-means run = 2
+kmeans2 <- kmeans(res.ca$li[,1:2], centers= 4 , nstart =2)
+print(kmeans)
 
-library(cluster)
-km.res <- eclust(res.ca$li[,1:2], "kmeans", k =4,
-                 nstart = 2, graph = FALSE) #nstart=1 meme résultat que 
-fviz_cluster(km.res, frame.type = "norm", frame.level = 0.68)
-#sil <- silhouette(km.res$cluster, dist(res.ca$li[,1:2],method="euclidian")^2)
-fviz_silhouette(km.res, label=TRUE)
+#Plot K-means run = 1
+km.res1 <- eclust(res.ca$li[,1:2], "kmeans", k =4,nstart = 1, graph = TRUE) #nstart=1 meme résultat que 
 
 
-res.hc <- eclust(res.ca$li[,1:2], "hclust", k = 4,
-                 method = "ward.D2", graph = FALSE)
-fviz_dend(res.hc, rect = TRUE, show_labels = TRUE) 
+#Plot K-means run = 2
+km.res2 <- eclust(res.ca$li[,1:2], "kmeans", k =4,nstart = 2, graph = TRUE) #nstart=1 meme résultat que 
+
+#Plot Silhouette run = 1
+fviz_silhouette(km.res1, label=TRUE)
+
+#Plot Silhouette run = 2
+fviz_silhouette(km.res1, label=TRUE)
+
+
 
